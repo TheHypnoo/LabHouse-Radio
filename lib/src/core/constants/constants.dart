2560e8e416895aea7c88f5da557f6f2774428db2
mysterlegendz@gmail.com <sergi@bambai.app>
2024-02-08 21:54:57 +0100

    Added image constants and API URLs in constants.dart

---
+++ lib/src/core/constants/constants.dart
@@ -0,0 +1,3 @@
+const String imageLabHouse =
+    'https://static.wixstatic.com/media/80a726_869df0ad73b64122bc296d07a7280ef9~mv2.png/v1/crop/x_800,y_934,w_2381,h_620/fill/w_424,h_110,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/LABHOUSE_Logo2_02.png';
+const String apiUrl = 'https://de1.api.radio-browser.info/json/stations/';
